0b5b713f6e07376786de722c757594fe7589fed3
5bug <30523953+5bug@users.noreply.github.com>
2025-03-20 12:22:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,10 +12,20 @@ func sort(arr []int) {
 	}
 }
 
+func sort2(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[i+1], arr[j] = arr[j], arr[j+1]
+			}
+		}
+	}
+}
+
 func main() {
 	var a []int
 	a = []int{1, 2, 3, 4, 5}
-	sort(a)
+	sort2(a)
 	fmt.Println(a)
-	fmt.Println("Hello Worle", b)
+	fmt.Sprintf("Hello Worle%s %d", b)
 }
